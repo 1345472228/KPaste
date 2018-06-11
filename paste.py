362a9acb72c0f63a6ed07e0d560003343c3f78ee
ykai <yingkaidang@gmail.com>
2018-06-12 01:35:21 +0800

    使用PEP 8规范

--- paste.py
+++ paste.py
@@ -1,27 +1,29 @@
-from flask import Flask, url_for, redirect, render_template, flash, request, g
-import db
 import threading
 from datetime import timedelta
+
+from flask import Flask, url_for, redirect, render_template, request
+
+import db
 
 app = Flask(__name__)
 db = db.DB()
 
 expired_check_timer = None
+
+
 def expired_check_timer_handler(db):
     global expired_check_timer
     expired_check_timer = threading.Timer(timedelta(1).total_seconds(), expired_check_timer_handler)
     expired_check_timer.start()
     db.check_validity()
 
-@app.before_request
-def begin_db(exception=None):
-    db.begin()
 
 @app.teardown_request
 def end_db(exception=None):
     db.end()
 
-@app.route('/canceltimer')
+
+@app.route('/timer/stop')
 def cancel_timer():
     try:
         if expired_check_timer and expired_check_timer.is_alive():
@@ -34,7 +36,8 @@ def cancel_timer():
     except:
         return "Canceltimer: some errors occured"
 
-@app.route('/starttimer')
+
+@app.route('/timer/start')
 def start_timer():
     db.begin()
     try:
@@ -51,6 +54,7 @@ def start_timer():
     finally:
         db.end()
 
+
 @app.route('/')
 @app.route('/new/')
 def new():
@@ -60,20 +64,24 @@ def new():
         7: 'one week',
         14: 'one fortnight'
     }
+    db.begin()
     langs = db.query_lang(lang_id=0, q_all=True)
     return render_template('new.html', languages=langs, days_opt=days_opt)
+
 
 @app.route('/show/')
 @app.route('/show/<int:post_id>')
 def show(post_id=None):
     if post_id:
-        PostAndLang= db.query_post(post_id=post_id)
+        db.begin()
+        PostAndLang = db.query_post(post_id=post_id)
         if PostAndLang:
             return render_template('show.html', PostAndLang=PostAndLang)
         else:
             return "No Such Post"
     else:
         return "Error post_id"
+
 
 @app.route('/create', methods=["POST"])
 def create():
@@ -82,11 +90,14 @@ def create():
         form['language'] = int(form['language'])
     except:
         return 'Error form'
+
+    db.begin()
     lang = db.query_lang(form['language'])
     if lang:
         newpost = db.add_post(**form)
         db.session.commit()
         return redirect(url_for('show', post_id=newpost.id))
+
 
 if __name__ == '__main__':
     start_timer()
